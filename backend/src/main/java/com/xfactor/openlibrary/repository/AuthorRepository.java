d6cab1b66015ef7a3cd2fba0077f1f4b41137694
SUBHADEEP DEY <107262066+DastaanSubha@users.noreply.github.com>
2023-04-08 00:56:03 +0530

    Delete backend/src directory

--- backend/src/main/java/com/xfactor/openlibrary/repository/AuthorRepository.java
+++
@@ -1,7 +0,0 @@
-package com.xfactor.openlibrary.repository;
-import com.xfactor.openlibrary.domain.Author;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository <Author, Integer>{
-    
-}
